788c35bdb06e20a0018b21a7b7a4e8ea4bd82e41
CoMatu <30466272+CoMatu@users.noreply.github.com>
2023-03-27 18:35:27 +0300

    first commit

---
+++ goHello.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	date := time.Date(2001, 12, 1, 0, 0, 0, 0, time.UTC)
+
+	switch y := date.Year(); {
+	case y >= 1946 && y <= 1965:
+		fmt.Println("Привет, бумер!")
+	case y >= 1965 && y < 1981:
+		fmt.Println("Привет, представитель X!")
+	case y >= 1981 && y < 1997:
+		fmt.Println("Привет, миллениал!")
+	case y >= 1997 && y < 2013:
+		fmt.Println("Привет, зумер!")
+	case y >= 2013:
+		fmt.Println("Привет, альфа!")
+	default:
+		fmt.Println("Здравствуйте!")
+	}
+
+	// создаём переменную
+	v := 0
+	for i := 1; i < 10; i++ {
+		// наращиваем переменную
+		v++
+	}
+	// выводим результат на экран
+	fmt.Println(v)
+
+	//fmt.Println(one, three, five, seven, nine, eleven)
+}
